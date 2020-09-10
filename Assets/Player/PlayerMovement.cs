44751021397b337a705376167897e499d38c91c9
Catherine Gilbert <catherine.gilbert15@gmail.com>
2020-09-10 14:42:34 -0400

    added basic movement and level

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Mathematics;
+
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float MaxVelocity = 1.0f;
+	public float Speed = 10.0f;
+
+	private float HorizontalVelocity;
+
+	private Rigidbody2D RigidBody;
+
+	private void Start()
+	{
+		RigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		HorizontalVelocity = Input.GetAxis("Horizontal") * Speed;
+	}
+
+	private void FixedUpdate()
+	{
+		float targetHorizontalVelocity = math.clamp(RigidBody.velocity.x + HorizontalVelocity, -MaxVelocity, MaxVelocity);
+
+		RigidBody.velocity = new Vector2(targetHorizontalVelocity, RigidBody.velocity.y);
+	}
+}
