6d5f2ae791dee8a23a07062e94e00ca2c845eb4b
Catherine Gilbert <catherine.gilbert15@gmail.com>
2020-09-22 18:02:04 -0400

    added sound effect system
    Added sounds when moving the character.
    Added ambiant music.

--- Assets/Player/Scripts/PlayerPlatformerController.cs
+++ Assets/Player/Scripts/PlayerPlatformerController.cs
@@ -47,5 +47,4 @@ public class PlayerPlatformerController : PhysicsObject
 		targetVelocity = move * maxSpeed;
 	}
 
-	private void PlayFootstep() { }
 }
---
+++ Assets/Utilities/SFXPlayer/Scripts/SoundEffect.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundEffect", menuName = "ScriptableObjects/SoundEffect", order = 1)]
+public class SoundEffect : ScriptableObject
+{
+    public AudioClip soundToPlay;
+}
