e6b3b9a1fa9451f66b58d38a1355f8419272bf1f
Catherine Gilbert <catherine.gilbert15@gmail.com>
2020-09-23 19:44:10 -0400

    more work on camera follow script

--- Assets/Camera/Scripts/FollowPlayer.cs
+++ Assets/Camera/Scripts/FollowPlayer.cs
@@ -4,6 +4,9 @@ public class FollowPlayer : MonoBehaviour
 {
     public float followSpeed = 10.0f;
     public float minDistance = 0.25f;
+    [Range(0.001f, 1.0f)]
+    public float stiffnessFactor = 0.25f;
+    public Vector3 offset = new Vector3(0.0f, 0.2f, 0.0f);
 
     private GameObject player;
 
@@ -18,8 +21,8 @@ public class FollowPlayer : MonoBehaviour
 
         Vector3 targetDirection = player.transform.position - cameraPosition;
         float velocity = targetDirection.magnitude <= minDistance ? 0.0f : targetDirection.magnitude * followSpeed;
-        Vector3 targetPos = cameraPosition + targetDirection.normalized * (velocity * Time.deltaTime);
+        Vector3 targetPos = cameraPosition + offset + targetDirection.normalized * (velocity * Time.deltaTime);
 
-         transform.position = Vector3.Lerp(cameraPosition, targetPos, 0.25f);
+        transform.position = Vector3.Lerp(cameraPosition, targetPos, stiffnessFactor);
     }
 }
