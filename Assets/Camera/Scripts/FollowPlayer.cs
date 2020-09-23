f7e05e6c01c4a55d49fd0817cad92a994c33a60a
Catherine Gilbert <catherine.gilbert15@gmail.com>
2020-09-23 18:09:17 -0400

    Added player following to camera
    moved external assets to "external" folder

---
+++ Assets/Camera/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public float followSpeed = 10.0f;
+    public float minDistance = 0.25f;
+
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition = transform.position;
+
+        Vector3 targetDirection = player.transform.position - cameraPosition;
+        float velocity = targetDirection.magnitude <= minDistance ? 0.0f : targetDirection.magnitude * followSpeed;
+        Vector3 targetPos = cameraPosition + targetDirection.normalized * (velocity * Time.deltaTime);
+
+         transform.position = Vector3.Lerp(cameraPosition, targetPos, 0.25f);
+    }
+}
