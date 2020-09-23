f22cf9e04129fc26471482d4cdef5c14a95dd48e
Catherine Gilbert <catherine.gilbert15@gmail.com>
2020-09-23 19:49:14 -0400

    changed FollowPlayer script to FollowTarget

--- Assets/Camera/Scripts/FollowTarget.cs
+++ Assets/Camera/Scripts/FollowTarget.cs
@@ -1,25 +1,28 @@
 ﻿using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class FollowTarget : MonoBehaviour
 {
     public float followSpeed = 10.0f;
     public float minDistance = 0.25f;
     [Range(0.001f, 1.0f)]
     public float stiffnessFactor = 0.25f;
     public Vector3 offset = new Vector3(0.0f, 0.2f, 0.0f);
-
-    private GameObject player;
+    public GameObject target;
 
     private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        // follow player if nothing is specified
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
     }
 
     private void FixedUpdate()
     {
         Vector3 cameraPosition = transform.position;
 
-        Vector3 targetDirection = player.transform.position - cameraPosition;
+        Vector3 targetDirection = target.transform.position - cameraPosition;
         float velocity = targetDirection.magnitude <= minDistance ? 0.0f : targetDirection.magnitude * followSpeed;
         Vector3 targetPos = cameraPosition + offset + targetDirection.normalized * (velocity * Time.deltaTime);
 
