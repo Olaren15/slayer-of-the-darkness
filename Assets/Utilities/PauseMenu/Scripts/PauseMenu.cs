06d095ad12aecdd632282f15f268efc34f8c61c8
Catherine Gilbert <catherine.gilbert15@gmail.com>
2020-09-23 20:14:15 -0400

    converted pause menu to new input system

---
+++ Assets/Utilities/PauseMenu/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool paused;
+    public GameObject pauseMenuUI;
+
+    private Controls controls;
+
+    private void Awake()
+    {
+        controls = new Controls();
+        controls.Enable();
+        controls.Interface.Pause.performed += context => TogglePauseMenu();
+    }
+
+    private void TogglePauseMenu()
+    {
+        if (paused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        paused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        paused = true;
+    }
+}
